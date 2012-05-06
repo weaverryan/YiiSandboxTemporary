5df65e3010b0b8a9efbc0194be1115b3044dfa04
Ryan Weaver <ryan@thatsquality.com>
2012-05-06 16:44:00 -0500

    Adding the whole framework to git, just for simplicity

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 // change the following paths if necessary
-$yii=dirname(__FILE__).'/../framework-yii-1.1.10/yii.php';
+$yii=dirname(__FILE__).'/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
 
 // remove the following line when in production mode
